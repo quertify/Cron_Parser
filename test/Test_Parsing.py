69d5dbdc5ec800646f27c91e121a12f8e72b5ecf
quertify <poojamodi084@gmail.com>
2024-06-10 15:50:09 +0100

    Added all the required files
    This is the Cron parser for extracting cron expressions

---
+++ test/Test_Parsing.py
@@ -0,0 +1,65 @@
+#Unit test file for Cron parser
+import unittest
+from Parsing import Parsing
+
+class Test_Parsing(unittest.TestCase):
+        def test_parse_valid_expression(self):
+        # Test that the parse method correctly parses a valid cron expression
+            testcase1 = Parsing("*/15 0 1,15 * 1-5 /usr/bin/find")
+            expected_output = [
+                ("minute", "0 15 30 45"),
+                ("hour", "0"),
+                ("day of month", "1 15"),
+                ("month", "1 2 3 4 5 6 7 8 9 10 11 12"),
+                ("day of week", "1 2 3 4 5"),
+                ("command", "/usr/bin/find")
+            ]
+            """ Test to check if a valid expression runs"""
+            self.assertEqual(testcase1.parse(), expected_output)
+
+        def test_invalid_expression(self):
+              testcase2 = Parsing("* * * * *")
+              with self.assertRaises(ValueError):
+                testcase2.parse()
+
+        
+        # Testing the ranges for all the fields
+
+        def test_parse_range_start_expression(self):
+        # Test that the parse method correctly parses a valid cron expression
+            testcase3 = Parsing("0 0 1 1 0 /user/parse/pin")
+            expected_output = [
+                ("minute", "0"),
+                ("hour", "0"),
+                ("day of month", "1"),
+                ("month", "1"),
+                ("day of week", "0"),
+                ("command", "/user/parse/pin")
+            ]
+            self.assertEqual(testcase3.parse(), expected_output)
+
+        def test_parse_range_end_expression(self):
+            testcase4 = Parsing("59 23 31 12 6 /user/parse/pin")
+            expected_output = [
+                ("minute", "59"),
+                ("hour", "23"),
+                ("day of month", "31"),
+                ("month", "12"),
+                ("day of week", "6"),
+                ("command", "/user/parse/pin")
+            ]
+            self.assertEqual(testcase4.parse(), expected_output)
+        def test_invalid_range_expression(self):
+            testcase5 = Parsing("-1 24 32 13 7 command")
+            with self.assertRaises(ValueError):
+                testcase5.parse()
+
+        def test_valid_cron_expression_with_steps(self):
+            testcase6 = Parsing("0 9/2 * * 1-5 /usr/bin/purge-temps")
+            with self.assertRaises(ValueError):
+                testcase6.parse()
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+              
---
+++ test/Test_Validation.py
@@ -0,0 +1,44 @@
+import unittest
+from Parsing import Parsing
+
+class Test_Validation(unittest.TestCase):
+    def test_valid_value_expression(self):
+        testcase1 = Parsing("0 * * * * /user/command")
+        expected_output = [
+                ("minute", "0"),
+                ("hour", "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23"),
+                ("day of month", "1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31"),
+                ("month", "1 2 3 4 5 6 7 8 9 10 11 12"),
+                ("day of week", "0 1 2 3 4 5 6"),
+                ("command", "/user/command")
+            ]
+        self.assertEqual(testcase1.parse(), expected_output)
+        pass
+
+    def test_invalid_character(self):
+        with self.assertRaisesRegex(ValueError, r"Invalid Character in the field: @1-5"):
+            Parsing("*/15 0 1,15 * @1-5 /usr/bin/command").validate_exp()
+        pass
+
+    def test_invalid_step(self):
+        with self.assertRaisesRegex(ValueError, r"Invalid split value : -15"):
+            Parsing("*/-15 0 1,15 * 1-5 /usr/bin/command").validate_exp()
+        pass
+
+    def test_value_out_of_range(self):
+        with self.assertRaisesRegex(ValueError, r"Invalid range interval: 1-40"):
+            Parsing("*/15 0 1-40 * 1-5 /usr/bin/command").validate_exp()
+        pass
+
+    def test_missing_fields(self):
+        with self.assertRaisesRegex(ValueError, r"Error: Invalid cron format, Field count should be 6"):
+            Parsing("0 * * * *").validate_exp()
+        pass
+
+    def test_empty_expression(self):
+        with self.assertRaisesRegex(ValueError,  r"Error: Invalid cron format, Field count should be 6" ):
+            Parsing("0 0 * * ").validate_exp()
+        pass
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
